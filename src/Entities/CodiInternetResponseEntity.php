032b444c9491ccb4158b4aa87b213c62c66b8f45
Jiří Nový <novy@grapesc.cz>
2017-12-12 15:22:00 +0100

    feature: add entities

---
+++ src/Entities/CodiInternetResponseEntity.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Ispa\CodiModule\Entity;
+
+
+class CodiInternetResponseEntity
+{
+
+	/** @var string */
+	public $technology;
+
+	/** @var FromToEntity */
+	public $speedUp;
+
+	/** @var FromToEntity */
+	public $speedDown;
+
+	/** @var FromToEntity */
+	public $price;
+
+	/** @var array */
+	public $additional = [];
+
+	/** @var string|null */
+	public $webLink;
+
+	/** @var string|null */
+	public $orderLink;
+
+	/** @var string|null */
+	public $promoText;
+
+}
---
+++ src/Entities/FromToEntity.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Ispa\CodiModule\Entity;
+
+
+class FromToEntity
+{
+
+	/** @var int|null */
+	public $from = null;
+
+	/** @var int|null */
+	public $to = null;
+
+
+	/**
+	 * @param int $from
+	 */
+	public function setFrom($from)
+	{
+		if (is_int($from)) {
+			$this->from = $from;
+		} elseif (ctype_digit($from) || is_float($from)) {
+			$this->from = (int)$from;
+		}
+	}
+
+
+	/**
+	 * @param int $to
+	 */
+	public function setTo($to)
+	{
+		if (is_int($to)) {
+			$this->to = $to;
+		} elseif (ctype_digit($to) || is_float($to)) {
+			$this->to = (int) $to;
+		}
+	}
+
+}
